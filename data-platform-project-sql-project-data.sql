ff1dec74dd4e012eb8dc8de3d7f77a3b30f909f7
kimura <creative@latona.io>
2023-05-16 17:41:35 +0900

    release

--- data-platform-project-sql-project-data.sql
+++ data-platform-project-sql-project-data.sql
@@ -1,30 +1,25 @@
 CREATE TABLE `data_platform_project_project_data`
 (
-	`Project`			            varchar(24) NOT NULL,
+	`Project`		                int(16) NOT NULL,
+	`ProjectCode`	                varchar(24) NOT NULL,
 	`OwnerBusinessPartner`          int(12) DEFAULT NULL,
 	`ProjectDescription`            varchar(200) DEFAULT NULL,
-	`ProjectProfileCode`            varchar(7) DEFAULT NULL,
+	`ProjectProfile`	            varchar(7) DEFAULT NULL,
 	`PlannedStartDate`              date DEFAULT NULL,
 	`PlannedEndDate`                date DEFAULT NULL,
+	`ActualStartDate`               date DEFAULT NULL,
+	`ActualEndDate`                 date DEFAULT NULL,
 	`ResponsiblePerson`             int(12) DEFAULT NULL,
 	`ResponsiblePersonName`         varchar(100) DEFAULT NULL,
 	`CreationDate`                  date DEFAULT NULL,
 	`LastChangeDate`                date DEFAULT NULL,
-	`BasicDatesLastScheduledDate`   date DEFAULT NULL,
-	`FcstdDatesLastScheduledDate`   date DEFAULT NULL,
-	`ForecastedStartDate`           date DEFAULT NULL,
-	`ForecastedEndDate`             date DEFAULT NULL,
 	`PlantBusinessPartner`          int(12) DEFAULT NULL,
 	`Plant`                         varchar(4) DEFAULT NULL,
-	`Currency`                      varchar(5) DEFAULT NULL,
-	`Language`                      varchar(2) DEFAULT NULL,
 
 	PRIMARY KEY (`Project`),
 
 	CONSTRAINT `DataPlatformProjectProjectDataOwnerBusinessPartner_fk` FOREIGN KEY (`OwnerBusinessPartner`) REFERENCES `data_platform_business_partner_general_data` (`BusinessPartner`),
-	CONSTRAINT `DataPlatformProjectProjectDataPlant_fk` FOREIGN KEY (`PlantBusinessPartner`, `Plant`) REFERENCES `data_platform_plant_general_data` (`BusinessPartner`, `Plant`),
-	CONSTRAINT `DataPlatformProjectProjectDataCurrency_fk` FOREIGN KEY (`Currency`) REFERENCES `data_platform_currency_currency_data` (`Currency`),
-	CONSTRAINT `DataPlatformProjectProjectDataLanguage_fk` FOREIGN KEY (`Language`) REFERENCES `data_platform_language_language_data` (`Language`)
+	CONSTRAINT `DataPlatformProjectProjectDataPlant_fk` FOREIGN KEY (`PlantBusinessPartner`, `Plant`) REFERENCES `data_platform_plant_general_data` (`BusinessPartner`, `Plant`)
 
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
